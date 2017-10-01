b70e8fd94ad801e50e2121e3c6ac0734379373af
Jon Dillman <jon@plays.tv>
2017-09-30 17:45:11 -0700

    resets css

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
+Default React build bootstrapped from [Create React App](https://github.com/facebookincubator/create-react-app).
+
+React 16
+Webpack 3
+Redux using ducks
+Airbnb eslint
+
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import './App.css';
+
+import './css/common/reset.css';
+import './css/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header" />
-      <main />
-      <footer />
+    <div className="app">
+      <Header />
+      <main className="app-main">Body</main>
+      <footer className="app-footer">Footer</footer>
     </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="app-header">
+      <section className="header-logo">header</section>
+      <section className="header-center" />
+      <section className="header-account" />
+    </header>
   );
 }
 
