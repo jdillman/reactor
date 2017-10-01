d7369a9116d3a3cbd21bbb2bd3b1520374ef617c
Jon Dillman <jon@plays.tv>
2017-09-30 17:14:22 -0700

    Enable eslint with airbnb ruleset

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// eslint-disable-next-line
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
 registerServiceWorker();
