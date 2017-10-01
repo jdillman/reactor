4565db05793e0dd3f0b1b86b4df4852a147aae6f
Jon <Jon@email>
2017-09-30 22:03:34 -0700

    Add example routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import 'css/common/reset.css';
 import 'css/App.css';
+
+function Home() {
+  return 'Home';
+}
+
+function Resume() {
+  return 'Resume';
+}
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/resume" component={Resume} />
+      </Switch>
       <main className="app-main">Body</main>
       <footer className="app-footer">Footer</footer>
     </div>
